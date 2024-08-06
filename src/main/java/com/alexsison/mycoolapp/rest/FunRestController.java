ea4538b5d6fb49efb24d11e3e0f38559f772873f
alexmortelsison <150509106+alexmortelsison@users.noreply.github.com>
2024-08-06 12:13:18 +0900

    updated main

--- src/main/java/com/alexsison/mycoolapp/rest/FunRestController.java
+++ src/main/java/com/alexsison/mycoolapp/rest/FunRestController.java
@@ -8,6 +8,16 @@ public class FunRestController {
 
     @GetMapping("/")
     public String sayHello() {
-        return "Hello World";
+        return "Hello World!";
+    }
+
+    @GetMapping("/workout")
+    public String getDailyWorkout() {
+        return "Run a hard 5k!";
+    }
+
+    @GetMapping("/fortune")
+    public String getDailyFortune() {
+        return "Today is your lucky day.";
     }
 }
